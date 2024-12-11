5db642740bd05565270c5986541dd5e17a040113
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-11 12:11:13 +0100

    feat(session.ts): added logic to update, verify, and delete session

--- src/app/actions/session.ts
+++ src/app/actions/session.ts
@@ -4,6 +4,7 @@ import { SessionPayload } from "../lib/definitions";
 import { SessionModel } from "../lib/schema";
 import mongoose from "mongoose";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const secretKey = process.env.SECRET_KEY;
 const key = new TextEncoder().encode(secretKey);
@@ -50,4 +51,32 @@ export async function createSession(id: mongoose.Types.ObjectId) {
     sameSite: "lax",
     path: "/",
   });
+}
+
+export async function updateSession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = await decrypt(session);
+
+  if (!session || !payload) return null;
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function verifySession() {
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+
+  if (!session?.useId) {
+    redirect("/login");
+  }
+
+  return { isAuth: true, userId: session.userId };
 }
